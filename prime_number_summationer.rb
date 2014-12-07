e374112b089707af2912473a26858048ef6052f3
Ricardo Quinones <ricardo@handybook.com>
2014-12-07 17:12:23 -0500

    Made some more optimizations

--- prime_number_summationer.rb
+++ prime_number_summationer.rb
@@ -18,13 +18,25 @@ class PrimeNumberSummationer
     max = nth_prime_number * 11
     odd_numbers = (3..max).step(2).to_a
     prime_number_count = 1
+    prime_numbers = []
 
     loop do
+      # Will include square of first number in array
+      index_to_take = odd_numbers.find_index { |num| num > odd_numbers.first ** 2 } + 1
+      primes_left = nth_prime_number - prime_number_count
+
+      if index_to_take - 1 >= primes_left
+        new_primes = odd_numbers[0...primes_left]
+        # Delete the square of the first prime number in the array if present
+        new_primes.delete(new_primes.first ** 2)
+        self.prime_number_sum += new_primes.inject(:+)
+        break
+      end
+
       new_prime = odd_numbers.shift
       prime_number_count += 1
+      prime_numbers << new_prime
       self.prime_number_sum += new_prime
-
-      break if prime_number_count == nth_prime_number
 
       odd_numbers -= ((new_prime ** 2)..max).step(new_prime).to_a
     end
