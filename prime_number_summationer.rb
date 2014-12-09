a16049f76ecce76a5c075ab9a30a3b97f33812e2
Ricardo Quinones <ricardo@handybook.com>
2014-12-08 22:52:58 -0500

    Uses odd number optimization

--- prime_number_summationer.rb
+++ prime_number_summationer.rb
@@ -15,24 +15,25 @@ class PrimeNumberSummationer
 
   def _calculate(nth_prime_number)
     initialize_calculator(nth_prime_number)
-    return 0 if nth_prime_number < 1
+    return if nth_prime_number < 1
 
     odd_numbers.each do |number|
       next unless number
       break if (self.prime_number_count += 1) == nth_prime_number
 
-      (number ** 2).step(upper_bound, number) do |non_prime|
-        odd_numbers[non_prime] = nil
+      (number ** 2).step(upper_bound, number * 2) do |non_prime|
+        odd_numbers[(non_prime - 1) / 2 - 1] = nil
       end
     end
 
-    self.prime_number_sum = odd_numbers.compact.take(nth_prime_number).inject(:+)
+    self.prime_number_sum = 2 + odd_numbers.compact.take(nth_prime_number - 1).inject(:+).to_i
   end
 
   def initialize_calculator(nth_prime_number)
     self.prime_number_count = 0
+    self.prime_number_sum = 0
     self.upper_bound = calculate_upper_bound(nth_prime_number)
-    self.odd_numbers = [nil, nil] + (2..upper_bound).to_a
+    self.odd_numbers = (3..upper_bound).step(2).to_a
   end
 
   def calculate_upper_bound(nth_prime_number)
